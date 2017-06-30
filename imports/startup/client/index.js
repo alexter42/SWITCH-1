db09e6aec1a34343dee482a6dd6fb001724f364d
LexieCore <lexie@protonmail.ch>
2017-06-30 19:35:30 +0200

    git /docs

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,6 +1,7 @@
 import './client-init.js';
 
 import '../../ui/containers/appContainer.scss';
+import '../../ui/containers/navigation/app.scss';
 import '../../ui/containers/navigation/menu.scss';
 import '../../ui/containers/navigation/dropzone.scss';
 import '../../ui/containers/navigation/grid.scss';
